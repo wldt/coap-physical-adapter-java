d103032ff757879b156a60ee34102a24587a8de0
Mirco Sandonini <mirco.ms16@gmail.com>
2023-11-20 12:14:52 +0100

    Added some package documentation

---
+++ src/main/java/it/wldt/adapter/coap/physical/resources/assets/core/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package containing the implementation of the CoAP resources based on the CoRE interface types.
+ */
+package it.wldt.adapter.coap.physical.resources.assets.core;
---
+++ src/main/java/it/wldt/adapter/coap/physical/resources/assets/functions/preprocessing/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package containing the body preprocessing functions that can be passed to the physical adapter by the user.
+ * These functions will be applied during the transaction between the CoAP protocol and the digital twin,
+ * making it possible to have the data in the same format independently of the protocol content format.
+ */
+package it.wldt.adapter.coap.physical.resources.assets.functions.preprocessing;
