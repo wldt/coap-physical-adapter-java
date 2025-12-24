0c0f3139214adb7b2ef24e078b26981c174ec0af
Marco Picone <picone.m@gmail.com>
2025-12-24 18:13:24 +0100

    Workflow initial commit & test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 group = "io.github.wldt"
-version = "0.1.1"
+version = "0.1.2"
 description = "Physical adapter to connect with the CoAP protocol"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
-
 plugins {
+    id("com.vanniktech.maven.publish") version "0.35.0"
     `java-library`
     `maven-publish`
     signing
@@ -25,7 +25,7 @@ dependencies {
 }
 
 java {
-    withJavadocJar()
+    //withJavadocJar()
     withSourcesJar()
 }
 
@@ -37,5 +37,50 @@ tasks.withType<Javadoc>() {
     options.encoding = "UTF-8"
     if (JavaVersion.current().isJava9Compatible) {
         (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
+
+// ✅ FIX ESATTO PER GRADLE 9.2.1 + vanniktech.maven.publish 0.35.0
+afterEvaluate {
+    val plainJavadocJarTask = tasks.findByName("plainJavadocJar")
+    val metadataTask = tasks.findByName("generateMetadataFileForMavenPublication")
+
+    if (plainJavadocJarTask != null && metadataTask != null) {
+        metadataTask.dependsOn(plainJavadocJarTask)
+    }
+}
+
+mavenPublishing {
+    coordinates(group.toString(), name.toString(), version.toString())
+
+    pom {
+        name.set("WLDT CoAP Physical Adapter")
+        description.set("The CoAP Physical Adapter for a WLDT Digital Twin")
+        inceptionYear.set("2025")
+        url.set("https://github.com/wldt/coap-physical-adapter-java")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("msandonini")
+                name.set("Mirco Sandonini")
+                url.set("https://github.com/msandonini")
+            }
+            developer {
+                id.set("piconem")
+                name.set("Marco Picone")
+                url.set("https://github.com/piconem")
+            }
+        }
+        scm {
+            url.set("https://github.com/wldt/coap-physical-adapter-java")
+            connection.set("scm:git:git://github.com/wldt/coap-physical-adapter-java.git")
+            developerConnection.set("scm:git:ssh://git@github.com/wldt/coap-physical-adapter-java.git")
+        }
     }
 }
